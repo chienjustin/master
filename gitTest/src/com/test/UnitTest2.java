b3cd12026d3b9cb67c5d43b73890ceeb41b8e34d
chienjustin <82429096+chienjustin@users.noreply.github.com>
2022-02-13 23:32:47 +0800

    Update UnitTest2.java
    888888

--- gitTest/src/com/test/UnitTest2.java
+++ gitTest/src/com/test/UnitTest2.java
@@ -3,6 +3,6 @@ package com.test;
 public class UnitTest2 {
 	public static void main(String args[]){
 		  
-		 System.out.println("=========test==123123===");
+		 System.out.println("=========test==88888===");
 	  }
 }
