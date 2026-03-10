d25a4a002fb040ce3ecde8b917cce872f51f6250
chienjustin <82429096+chienjustin@users.noreply.github.com>
2026-03-10 21:50:27 +0800

    renew

--- gitTest/src/com/test/UnitTest.java
+++
@@ -1,8 +0,0 @@
-package com.test;
-
-public class UnitTest {
-  public static void main(String args[]){
-	  
-	 System.out.println("=========test==20220210==989=");
-  }
-}
--- gitTest/src/com/test/UnitTest3.java
+++
@@ -1,8 +0,0 @@
-package com.test;
-
-public class UnitTest3 {
-	public static void main(String args[]){
-		  
-		 System.out.println("=========test==UnitTest3===");
-	  }
-}
